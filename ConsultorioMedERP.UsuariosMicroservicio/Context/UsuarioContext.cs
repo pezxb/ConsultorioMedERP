4bcb92cf36d47db880c6c95d8820150151317e97
pezxb <pezxb00@gmail.com>
2019-08-13 09:57:24 -0700

    microservicio usuario

---
+++ ConsultorioMedERP.UsuariosMicroservicio/Context/UsuarioContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using ConsultorioMedERP.Common.EstructuraOrganizacional;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultorioMedERP.UsuariosMicroservicio.Context
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Usuario> Usuario{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+        }
+    }
+}
---
+++ ConsultorioMedERP.UsuariosMicroservicio/Controllers/UsuariosController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConsultorioMedERP.UsuariosMicroservicio.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultorioMedERP.UsuariosMicroservicio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly UsuarioContext usuarioContext;
+
+        public UsuariosController(UsuarioContext usuarioContext)
+        {
+            this.usuarioContext = usuarioContext;
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var  resultado = await usuarioContext.Usuario.FirstOrDefaultAsync();
+                if(resultado != null)
+                {
+                    return Ok(resultado);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+    }
+}
